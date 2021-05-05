0de7abe72931b719cf712659734be00c98a95f9e
chahyeonho <chamym@naver.com>
2021-05-06 00:32:12 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { Fragment } from "react"; // Fragment기능을 사용
+import "./App.css";
 
 function App() {
+  const name = "리액트";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {" "}
+      {/* h1에 부모요소인 div가 있어야함 div를 쓰고싶지 않다면 리액트 v16 이상부터 도입된 Fragment기능을 사용 */}
+      {/* {name != '호스타' ? (<h1>리액트 입니다.</h1>) : (<h1>{name} 안녕?</h1>)} 엘리먼트에 if문이 없으므로 연산자로 사용 */}
+      <form>
+        성:<input></input>
+        <br />
+        이름:<input></input>
+        <br /> {/* 리액트에서는 인풋태그를 꼭 닫아줘야함 */}
+      </form>
+      <div className="react">{name}</div>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react';  // node_modules라는 디렉터리에 react모듈이 있고 이것을 임포트함
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+    // React.StrictMode 리액트의 레거시 기능을 사용 못하게하는 기능( ref, componentWillMount 등 나중에 사라지게될 옛날 기능을 사용했을시 경고 )
   <React.StrictMode>
     <App />
   </React.StrictMode>,
