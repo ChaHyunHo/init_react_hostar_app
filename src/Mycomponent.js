08270cfb3b45361ac03ac05903502b59e16c61eb
chahyeonho <chamym@naver.com>
2021-05-06 01:07:21 +0900

    new component commit

--- src/Mycomponent.js
+++ src/Mycomponent.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 // 새로운 컴포넌트 추가연습
-const Mycomponent = () => {
-  return <div>새로운 컴포넌트 생성 연습</div>;
+const Mycomponent = props => {
+  return <div className="react">새로운 컴포넌트 생성 연습 props name: {props.name}</div>;
 };
 
 export default Mycomponent;
