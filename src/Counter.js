87f6c8e8c8480db0beec9cd2a455ec9167f15963
chahyeonho <chamym@naver.com>
2021-05-08 23:02:42 +0900

    commit

--- src/Counter.js
+++ src/Counter.js
@@ -15,6 +15,7 @@ class Counter extends Component {
 
   render() {
     const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회
+
     return (
       <div>
         <h1>{number}</h1>
@@ -22,11 +23,18 @@ class Counter extends Component {
 
         <button
           onClick={() => {
-            this.setState((prevState) => {
-              return {  // 아래와 똑같음 다만 아래는 함수에서 바로 객체를 반환한다는 의미임
-                number: prevState.number + 1,
-              };
-            });
+            this.setState(
+              (prevState) => {
+                return {
+                  // 아래와 똑같음 다만 아래는 함수에서 바로 객체를 반환한다는 의미임
+                  number: prevState.number + 1,
+                };
+              },
+              () => { // this.setState 작업이 끝나고 득정 작업 실행시
+                console.log("setState 호출");
+                console.log(this.state);
+              }
+            );
 
             this.setState((prevState) => ({
               number: prevState.number + 1,
